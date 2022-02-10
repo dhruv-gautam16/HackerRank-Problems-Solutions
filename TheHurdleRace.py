26537c7eebedafcb91acef48da0a7b539075e351
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-02-11 00:08:34 +0530

    Solution for The Hurdle Race

---
+++ TheHurdleRace.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'hurdleRace' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY height
+#
+
+
+
+if __name__ == '__main__':
+
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    height = list(map(int, input().rstrip().split()))
+
+    d=max(height)
+    if(k>d):
+        print(0)
+    else:
+        print(abs(k-d))
