9b14378413d40340705b621152df09b48568aa10
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-03-10 01:01:56 +0530

    Solution for Mutations

---
+++ Mutations.py
@@ -0,0 +1,11 @@
+def mutate_string(string, position, character):
+    # Mutations in Python - HackerRank Solution START
+    pos = position+1
+    Output = string[:position]+character+string[pos:]
+    return Output
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
