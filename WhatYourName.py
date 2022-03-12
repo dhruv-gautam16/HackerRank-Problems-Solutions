abeca769abe16368f911c20773303a30026571b6
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-03-12 22:49:23 +0530

    Solution for What's your name?

---
+++ WhatYourName.py
@@ -0,0 +1,11 @@
+#
+# Complete the 'print_full_name' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING first
+#  2. STRING last
+#
+
+def print_full_name(a, b):
+    print("Hello {} {}! You just delved into python.".format(a,b))
