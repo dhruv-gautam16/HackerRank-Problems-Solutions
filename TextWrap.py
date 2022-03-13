175de596c3e877629a01c6a0cd3ed27afd561930
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-03-13 22:13:23 +0530

    Solution for Text Wrap

---
+++ TextWrap.py
@@ -0,0 +1,5 @@
+
+def wrap(string, max_width):
+    # Text Wrap in Python - HackerRank Solution START
+    return textwrap.fill(string,max_width)
+    # Text Wrap in Python - HackerRank Solution END
