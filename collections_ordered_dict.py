352cb26fd791bab11a54645eef2e02c289a515b4
dhruv-gautam 16 <dhruv.gautam@btech.christuniversity.in>
2022-03-18 00:05:04 +0530

    Solution for Collections.OrderedDict()

---
+++ collections_ordered_dict.py
@@ -0,0 +1,7 @@
+from collections import OrderedDict
+d = OrderedDict()
+for _ in range(int(input())):
+    item, space, quantity = input().rpartition(' ')
+    d[item] = d.get(item, 0) + int(quantity)
+for item, quantity in d.items():
+    print(item, quantity)
